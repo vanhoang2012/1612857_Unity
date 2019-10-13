f459d5187dbb4f7f762834139e87ae5f6bbb3a86
Văn Hoằng Trương <tvhoang2112@gmail.com>
2019-10-13 15:33:53 +0700

    done jump function

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Vector3 originalPos;
+    private Transform playerTransform;
+    private float startPoint;
+    private float tempX ;
+    void Start()
+    {
+        originalPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        startPoint = originalPos.x;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPos = playerTransform.transform.position;
+        if (Cameramoving()) {
+            movingFunc(newPos);
+        }
+    }
+
+    private void movingFunc(Vector3 newPos)
+    {
+        Vector3 cameraPos;
+        float newDis = newPos.x - originalPos.x;
+        cameraPos.x = transform.position.x + newDis;
+        cameraPos.y = transform.position.y;
+        cameraPos.z = transform.position.z;
+        transform.position = cameraPos;
+        originalPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    private bool Cameramoving()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>().flipX) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+        
+}
---
+++ Assets/MarioMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioMovement : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float jump = 500.0f;
+    public bool jumping = true;
+    public int numberOfJump = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move left, right
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetTrigger("Run");
+            numberOfJump = 0;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Animator>().SetTrigger("Run");
+            numberOfJump = 0;
+        }
+        // Jump
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!jumping && numberOfJump < 2)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jump));
+                numberOfJump++;
+                jumping = true;
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumping &= !collision.gameObject.name.Contains("Ground");
+    }
+}
